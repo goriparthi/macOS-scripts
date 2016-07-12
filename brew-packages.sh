b4c1b5b73e3e9953d7a1ca6abfd45173ef6813a4
Prashanth Goriparthi <prashanth@wrong.host>
2016-07-11 23:54:44 -0600

    Added cask and packages deployment using cask.

---
+++ brew-packages.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Brew packages that I use a lot.
+#
+brew install ansible
+brew install ansiweather
+brew install byobu
+brew install dos2unix
+brew install git
+brew install htop
+brew install mercurial
+brew install packer
+brew install ssh-copy-id
+brew install thefuck
+brew install tmux
+brew install tree
+brew install watch
+brew install wget
+
+# Brew - cask
+brew install brew-cask
+
+# Some cask packages that I use in my workflow.
+#
+brew cask install alfred
+brew cask install atom
+brew cask install caffeine
+brew cask install cheatsheet
+brew cask install controlplane
+brew cask install filezilla
+brew cask install gimp
+brew cask install gitup
+brew cask install google-chrome
+brew cask install handbrake
+brew cask install inkscape
+brew cask install iterm2
+brew cask install macdown
+brew cask install macvim
+brew cask install skype
+brew cask install slack
+brew cask install spectacle
+brew cask install sublime-text
+brew cask install teamviewer
+brew cask install tunnelblick
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+brew cask install the-unarchiver
+brew cask install xquartz
---
+++ mac-settings-changes.sh
@@ -0,0 +1,12 @@
+# Disable Game Center launching
+sudo defaults write /System/Library/LaunchAgents/com.apple.gamed Disabled -bool true
+# Avoid creation of .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# Disable autocorrect
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Enable dark mode
+defaults write NSGlobalDomain AppleInterfaceStyle Dark; killall Dock
+# Ask for password screen lock delay
+defaults -currentHost write com.apple.screensaver askForPasswordDelay -int 10
+# Allow app installation from any source
+sudo spctl --master-disable
